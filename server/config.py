03d143aeb766dff0a3421610e48493e10f23623d
baimixiaofan <baimeixiaofan@gmail.com>
2026-04-29 14:29:06 +0800

    修改密码

--- server/config.py
+++ server/config.py
@@ -5,6 +5,6 @@ import os
 # MySQL 数据库配置
 MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
 MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
-MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
+MYSQL_USER = os.getenv('MYSQL_USER', 'air_user')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'abc123456')
 MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'air_quality')
